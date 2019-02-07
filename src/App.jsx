120fe6338ab8df4bdd331ed2e17b6a36d622fbc9
Eugene Furmanov <codeinsack@gmail.com>
2019-02-08 00:01:38 +0300

    basic settings

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const app = () => {
+  return <div>Hello from Dixit!</div>;
+};
+
+export default app;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 2033,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  }
+};
